2fd7283febac9b317e7d7ddbdd2f78e933feb325
yousif <yousif.dhneem@gmail.com>
2023-12-10 15:40:01 +0300

    Events crud operations finished just need grab date in edit page

--- routes/event.js
+++ routes/event.js
@@ -4,7 +4,14 @@ const express = require('express');
 const router = express.Router();
 // Require event controller
 const eventCtrl = require('../controllers/event');
-router.use(express.urlencoded({extended: true}));
+
+router.get('/add', eventCtrl.event_create_get);
+router.post('/add', eventCtrl.event_create_post);
+router.get('/index', eventCtrl.events_index_get);
+router.get('/detail', eventCtrl.event_show_get);
+router.get('/delete', eventCtrl.event_delete_get);
+router.get('/edit', eventCtrl.event_edit_get);
+router.post('/update', eventCtrl.event_edit_post);
 
 
 
