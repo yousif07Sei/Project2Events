dc51688c6e553320743292065c2557e5965c3868
osamajanahi <osamaj12@gmail.com>
2023-12-12 16:06:44 +0300

    Done with the authorization

--- routes/category.js
+++ routes/category.js
@@ -4,20 +4,21 @@ const express = require('express');
 const router = express.Router();
 // Require category controller
 const categoryCtrl = require('../controllers/category');
+const isLoggedInAdmin = require('../config/isLoggedInAdmin');
 router.use(express.urlencoded({extended: true}));
 
-router.get('/add', categoryCtrl.category_add_get);
+router.get('/add', isLoggedInAdmin, categoryCtrl.category_add_get);
 
-router.post('/add', categoryCtrl.category_add_post);
+router.post('/add', isLoggedInAdmin, categoryCtrl.category_add_post);
 
 router.get('/index', categoryCtrl.category_index_get);
 
 router.get('/detail', categoryCtrl.category_show_get);
 
-router.get('/delete', categoryCtrl.category_delete_get);
+router.get('/delete', isLoggedInAdmin, categoryCtrl.category_delete_get);
 
-router.get('/edit', categoryCtrl.category_edit_get);
+router.get('/edit', isLoggedInAdmin, categoryCtrl.category_edit_get);
 
-router.post('/update', categoryCtrl.category_update_post);
+router.post('/update', isLoggedInAdmin, categoryCtrl.category_update_post);
 
 module.exports = router;
--- routes/event.js
+++ routes/event.js
@@ -2,11 +2,12 @@
 const express = require('express');
 // Initialize router functionality from express framework.
 const router = express.Router();
+const isLoggedInAdmin = require('../config/isLoggedInAdmin')
 // Require event controller
 const eventCtrl = require('../controllers/event');
 
-router.get('/add', eventCtrl.event_create_get);
-router.post('/add', eventCtrl.event_create_post);
+router.get('/add', isLoggedInAdmin, eventCtrl.event_create_get);
+router.post('/add', isLoggedInAdmin, eventCtrl.event_create_post);
 router.get('/index', eventCtrl.events_index_get);
 router.get('/detail', eventCtrl.event_show_get);
 router.get('/delete', eventCtrl.event_delete_get);
@@ -15,6 +16,8 @@ router.post('/update', eventCtrl.event_edit_post);
 router.post('/review', eventCtrl.event_review_post);
 router.post('/review/edit', eventCtrl.review_edit_post);
 router.post('/review/delete', eventCtrl.review_delete_post);
+
+
 
 
 
