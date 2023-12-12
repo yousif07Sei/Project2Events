a52b89d4c8f914c5004a7dbf67cda142ba846bd2
yousif <yousif.dhneem@gmail.com>
2023-12-12 15:46:13 +0300

    feature-multer-cloudinary/yousif-Osama

--- routes/event.js
+++ routes/event.js
@@ -4,9 +4,25 @@ const express = require('express');
 const router = express.Router();
 // Require event controller
 const eventCtrl = require('../controllers/event');
+// Require the Multer module
+const multer  = require('multer')
+// The file will hold the images
+// const upload = multer({ dest: 'public/images/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, uniqueSuffix  + '-' + file.originalname )
+    }
+  })
+
+const upload = multer({ storage: storage })
 
 router.get('/add', eventCtrl.event_create_get);
-router.post('/add', eventCtrl.event_create_post);
+router.post('/add', upload.array('image',5), eventCtrl.event_create_post);
 router.get('/index', eventCtrl.events_index_get);
 router.get('/detail', eventCtrl.event_show_get);
 router.get('/delete', eventCtrl.event_delete_get);
