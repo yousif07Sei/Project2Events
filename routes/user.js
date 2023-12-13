d36925f922cc2ec33cf282a464572e1e0a85cf82
yousif <yousif.dhneem@gmail.com>
2023-12-13 16:54:23 +0300

    move the config of the multer-cloudinary config is seprate file

--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,9 @@ const router = express.Router();
 // Require user controller
 const userCtrl = require('../controllers/user');
 const isLoggedInAdmin = require('../config/isLoggedInAdmin');
+const upload = require('../config/multerConfig');
 
-const multer  = require('multer')
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/images')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, uniqueSuffix  + '-' + file.originalname )
-    }
-})
-const upload = multer({ storage: storage })
-
+ 
 router.get('/edit', isLoggedInAdmin, userCtrl.user_edit_get);
 router.post('/update', isLoggedInAdmin,upload.single('avatar'), userCtrl.user_edit_post);
 
